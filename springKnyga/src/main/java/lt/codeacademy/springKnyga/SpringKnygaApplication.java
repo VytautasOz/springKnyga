dbce73c3e5e912149a75d66562299d62c422e217
VytautasOz <patrikasv@gmail.com>
2025-03-05 20:47:25 +0200

    Initial commit

---
+++ springKnyga/src/main/java/lt/codeacademy/springKnyga/SpringKnygaApplication.java
@@ -0,0 +1,25 @@
+package lt.codeacademy.springKnyga;
+
+import lombok.RequiredArgsConstructor;
+import lt.codeacademy.springKnyga.service.KnygaService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class SpringKnygaApplication {
+
+	private final KnygaService knygaService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKnygaApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void test(){
+		knygaService.addTestKnyga();
+	}
+
+}
---
+++ springKnyga/src/test/java/lt/codeacademy/springKnyga/SpringKnygaApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.springKnyga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKnygaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
